40f039e72a69d5f92985558e233d2d470cd2b08b
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2011-09-07 10:44:57 +0200

    [sudo] sudo::defaults::spec now with both 'content' and 'source' attributes
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- manifests/definitions/sudo-defaults-spec.pp
+++ manifests/definitions/sudo-defaults-spec.pp
@@ -15,7 +15,16 @@
 # == Parameters:
 #
 # [*content*]
-#  content of the Defaults directive (it SHOULD start by default)
+#  Specify the contents of the Defaults directive as a string. Newlines, tabs,
+#  and spaces can be specified using the escaped syntax (e.g., \n for a newline)
+#  Note that each line SHOULD start by 'Defaults' -- see sudoers(5)
+#
+# [*source*]
+#  Copy a file as the content of the Defaults directive.
+#  Uses checksum to determine when a file
+#  should be copied. Valid values are either fully qualified paths to files, or
+#  URIs. Currently supported URI types are puppet and file.
+#  If content was not specified, you are expected to use the source
 #
 # == Examples
 #
@@ -43,27 +52,36 @@
 #
 # [Remember: No empty lines between comments and class definition]
 #
-define sudo::defaults::spec($content='') {
+define sudo::defaults::spec($content='', $source='') {
 
     include sudo::params
 
     # $name is provided by define invocation
     # guid of this entry
     $defaultname = $name
-
-    $real_content = $content ? {
-        ''      => undef,
-        default => $content,
+    
+    # if content is passed, use that, else if source is passed use that
+    case $content {
+        '': {
+            case $source {
+                '': {
+                    crit('No content nor source have been  specified')
+                }
+                default: { $real_source = $source }
+            }
+        }
+        default: { $real_content = $content }
     }
 
     concat::fragment { "sudoers_defaults_spec_${defaultname}":
         target  => "${sudo::params::configfile}",
-        content => $real_content,
         ensure  => "${sudo::ensure}",
         order   => 65,
+        content => $real_content,
+        source  => $real_source,
         notify  => Exec["${sudo::params::check_syntax_name}"],
     }
-    
+
 }
 
 
