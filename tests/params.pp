fcdc5ad1ade7f91dff6bb8194cc4c8b90e65daa8
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-09-08 12:38:17 -0500

    update content of 'params.pp'
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- tests/params.pp
+++ tests/params.pp
@@ -1,13 +1,13 @@
-# File::      <tt>params.pp</tt> 
-# Author::    Sebastien Varrette (<Sebastien.Varrette@uni.lu>)
-# Copyright:: Copyright (c) 2014 Sebastien Varrette (www[http://varrette.gforge.uni.lu])
-# License::   GPLv3
-# 
+# File::      <tt>params.pp</tt>
+# Author::    S. Varrette, H. Cartiaux, V. Plugaru (hpc-sysadmins@uni.lu)
+# Copyright:: Copyright (c) 2014 S. Varrette, H. Cartiaux, V. Plugaru
+# License::   Gpl-3.0
+#
 # ------------------------------------------------------------------------------
 # You need the 'future' parser to be able to execute this manifest (that's
 # required for the each loop below).
 #
-# Thus execute this manifest as follows:
+# Thus execute this manifest in your vagrant box as follows:
 #
 #      sudo puppet apply -t --parser future /vagrant/tests/params.pp
 #
@@ -15,15 +15,9 @@
 
 include 'sudo::params'
 
-$names = [
-          'packagename', 'configfile', 'backupconfigfile',
-          'configfile_mode', 'configfile_owner', 'configfile_group',
-          'configdir', 'configdir_mode', 'configdir_group',
-          'check_syntax_name', 'cmdalias_pkgmanager'
-          ]
+$names = ["ensure ", "packagename ", "configfile ", "backupconfigfile ", "configfile_mode ", "configfile_owner ", "configfile_group ", "configdir ", "configdir_mode ", "configdir_owner ", "configdir_group ", "check_syntax_name ", "cmdalias_pkgmanager "]
 
 each($names) |$v| {
     $var = "sudo::params::${v}"
     notice("${var} = ", inline_template('<%= scope.lookupvar(@var) %>'))
 }
-
