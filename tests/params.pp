f32ca53a263540223af7975d609a422f56da9079
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-09-08 14:19:27 -0500

    update content of 'params.pp'
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- tests/params.pp
+++ tests/params.pp
@@ -17,6 +17,20 @@ include 'sudo::params'
 
 $names = ["ensure ", "packagename ", "configfile ", "backupconfigfile ", "configfile_mode ", "configfile_owner ", "configfile_group ", "configdir ", "configdir_mode ", "configdir_owner ", "configdir_group ", "check_syntax_name ", "cmdalias_pkgmanager "]
 
+notice("sudo::params::ensure  = ${sudo::params::ensure }")
+notice("sudo::params::packagename  = ${sudo::params::packagename }")
+notice("sudo::params::configfile  = ${sudo::params::configfile }")
+notice("sudo::params::backupconfigfile  = ${sudo::params::backupconfigfile }")
+notice("sudo::params::configfile_mode  = ${sudo::params::configfile_mode }")
+notice("sudo::params::configfile_owner  = ${sudo::params::configfile_owner }")
+notice("sudo::params::configfile_group  = ${sudo::params::configfile_group }")
+notice("sudo::params::configdir  = ${sudo::params::configdir }")
+notice("sudo::params::configdir_mode  = ${sudo::params::configdir_mode }")
+notice("sudo::params::configdir_owner  = ${sudo::params::configdir_owner }")
+notice("sudo::params::configdir_group  = ${sudo::params::configdir_group }")
+notice("sudo::params::check_syntax_name  = ${sudo::params::check_syntax_name }")
+notice("sudo::params::cmdalias_pkgmanager  = ${sudo::params::cmdalias_pkgmanager }")
+
 each($names) |$v| {
     $var = "sudo::params::${v}"
     notice("${var} = ", inline_template('<%= scope.lookupvar(@var) %>'))
